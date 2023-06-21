214214f91153d5d3103da34fd6331cf1feb7e252
unknown <jeewanthamsenanayake@gmail.com>
2023-06-21 22:58:10 +0530

    dashboard done

--- src/components/Students/DashboardComp.vue
+++ src/components/Students/DashboardComp.vue
@@ -10,16 +10,17 @@
             <v-card-subtitle class="text-left">{{ data.subTopic }}</v-card-subtitle>
             <v-card-text>
               <div class="text-left">
-              <v-btn  color="orange lighten-1" small @click="openZoomLink(data.zoom)">Join class</v-btn>
+              <v-btn v-if="data.isFree==1" :class="!$vuetify.breakpoint.xs?'ml-2 mb-2 white--text':'mb-2 white--text'" color="green " small @click="openZoomLink(data.zoom)">Join class</v-btn>
+              <v-btn v-else :disabled="data.paymentDone==1" :class="!$vuetify.breakpoint.xs?'ml-2 mb-2 white--text':'mb-2 white--text'" :color="data.paymentDone==0?'orange lighten-1':data.paymentDone==1?'orange lighten-1':'green'" small @click="openZoomLink(data.zoom)">{{ data.paymentDone==0?"enroll class":data.paymentDone==1?"pending":"Join class" }}</v-btn>
             </div>
               <div v-if="data.tutes.length > 0" class="text-left">
-                <v-row v-for="(tutesData,m) in data.tutes" :key="m" :class="!$vuetify.breakpoint.xs?'blue lighten-5 my-1':'white my-1' ">
+                <v-row v-for="(tutesData,m) in data.tutes" :key="m" :class="!$vuetify.breakpoint.xs?'blue lighten-5 my-1 mx-2':'white my-1 mr-3' ">
                   <v-col cols="9" md="10" sm="10">
-                    <h4>
+                     <h4>
                     {{ tutesData.name }}</h4>
                   </v-col>
                   <v-col cols="1" md="2" sm="2" class="text-right">
-                     <v-btn class="" x-small color="primary" @click="downloadPDF(tutesData.link)">Download</v-btn>
+                     <v-btn :disabled="(data.paymentDone==0 || data.paymentDone==1)&& data.isFree !=1" class="" x-small color="primary" @click="downloadPDF(tutesData.link)">Download</v-btn>
                   </v-col>
                 </v-row>
               </div>
@@ -41,9 +42,10 @@ export default {
   data() {
     return {
       classData: [
-        {
-          "isFree": 0,
-          "topic": "Combined Maths 2023 (A/L)",
+        { "classId":1,
+          "isFree": 1,
+          "paymentDone":0,
+          "topic": "Combined Maths 2023 (A/L) - 1",
           "subTopic": "Differentiation",
           "tutes": [
             {
@@ -57,11 +59,55 @@ export default {
           ],
           "zoom":"https://us04web.zoom.us/j/71918014924?pwd=8g9saFpaNsZAqlnBWUKrAxmiEG6NRL.1"
         },
-        {
+        {"classId":2,
           "isFree": 0,
-          "topic": "Combined Maths 2023 (A/L)",
+          "paymentDone":0,
+          "topic": "Combined Maths 2023 (A/L) - 2",
           "subTopic": "Differentiation",
-          "tutes": [],
+          "tutes": [
+            {
+              "name": "Differentiation Tute 1",
+              "link": "https://firebasestorage.googleapis.com/v0/b/cmp--academy.appspot.com/o/Tutes%2Flesson8.pdf?alt=media&token=97b1a155-80f9-49d6-be26-dcb837c8f105"
+            },
+            {
+              "name": "Differentiation Tute 2",
+              "link": "https://firebasestorage.googleapis.com/v0/b/cmp--academy.appspot.com/o/Tutes%2Flesson8.pdf?alt=media&token=97b1a155-80f9-49d6-be26-dcb837c8f105"
+            }
+          ],
+          "zoom":"https://us04web.zoom.us/j/71918014924?pwd=8g9saFpaNsZAqlnBWUKrAxmiEG6NRL.1"
+        },
+        {"classId":3,
+          "isFree": 0,
+          "paymentDone":1,
+          "topic": "Combined Maths 2023 (A/L) - 3",
+          "subTopic": "Differentiation",
+          "tutes": [
+            {
+              "name": "Differentiation Tute 1",
+              "link": "https://firebasestorage.googleapis.com/v0/b/cmp--academy.appspot.com/o/Tutes%2Flesson8.pdf?alt=media&token=97b1a155-80f9-49d6-be26-dcb837c8f105"
+            },
+            {
+              "name": "Differentiation Tute 2",
+              "link": "https://firebasestorage.googleapis.com/v0/b/cmp--academy.appspot.com/o/Tutes%2Flesson8.pdf?alt=media&token=97b1a155-80f9-49d6-be26-dcb837c8f105"
+            }
+          ],
+          "zoom":"https://us04web.zoom.us/j/71918014924?pwd=8g9saFpaNsZAqlnBWUKrAxmiEG6NRL.1"
+        },
+        {"classId":4,
+          "isFree": 0,
+          "paymentDone":2,
+          "topic": "Combined Maths 2023 (A/L) - 4",
+          "subTopic": "Differentiation",
+          "tutes": [
+            {
+              "name": "Differentiation Tute 1",
+              "link": "https://firebasestorage.googleapis.com/v0/b/cmp--academy.appspot.com/o/Tutes%2Flesson8.pdf?alt=media&token=97b1a155-80f9-49d6-be26-dcb837c8f105"
+            },
+            {
+              "name": "Differentiation Tute 2",
+              "link": "https://firebasestorage.googleapis.com/v0/b/cmp--academy.appspot.com/o/Tutes%2Flesson8.pdf?alt=media&token=97b1a155-80f9-49d6-be26-dcb837c8f105"
+            }
+          ],
           "zoom":"https://us04web.zoom.us/j/71918014924?pwd=8g9saFpaNsZAqlnBWUKrAxmiEG6NRL.1"
         }
       ]
